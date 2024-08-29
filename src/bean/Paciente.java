b6908caa5d5e5b1320d0e1c1116e8140578aa534
DaviCarvalhoo <davicarvalhotech@gmail.com>
2024-08-29 13:42:12 -0300

    adding Jframe CadPaciente

---
+++ src/bean/Paciente.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Davi
+ */
+@Entity
+@Table(name = "paciente")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Paciente.findAll", query = "SELECT p FROM Paciente p")
+    , @NamedQuery(name = "Paciente.findByIdpaciente", query = "SELECT p FROM Paciente p WHERE p.idpaciente = :idpaciente")
+    , @NamedQuery(name = "Paciente.findByNome", query = "SELECT p FROM Paciente p WHERE p.nome = :nome")
+    , @NamedQuery(name = "Paciente.findByCpf", query = "SELECT p FROM Paciente p WHERE p.cpf = :cpf")})
+public class Paciente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idpaciente")
+    private Integer idpaciente;
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "cpf")
+    private String cpf;
+    
+    @Transient
+    private transient final java.beans.PropertyChangeSupport propertyChangeSupport = new java.beans.PropertyChangeSupport(this);
+
+    /**
+     * Add PropertyChangeListener.
+     *
+     * @param listener
+     */
+    public void addPropertyChangeListener(java.beans.PropertyChangeListener listener )
+    {
+        propertyChangeSupport.addPropertyChangeListener( listener );
+    }
+
+    /**
+     * Remove PropertyChangeListener.
+     *
+     * @param listener
+     */
+    public void removePropertyChangeListener(java.beans.PropertyChangeListener listener )
+    {
+        propertyChangeSupport.removePropertyChangeListener( listener );
+    }
+
+    public Paciente() {
+    }
+
+    public Paciente(Integer idpaciente) {
+        this.idpaciente = idpaciente;
+    }
+
+    public Integer getIdpaciente() {
+        return idpaciente;
+    }
+
+    public void setIdpaciente(Integer idpaciente) {
+        Integer oldIdpaciente = this.idpaciente;
+        this.idpaciente = idpaciente;
+        propertyChangeSupport.firePropertyChange("idpaciente", oldIdpaciente, idpaciente);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        String oldNome = this.nome;
+        this.nome = nome;
+        propertyChangeSupport.firePropertyChange("nome", oldNome, nome);
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        String oldCPF = this.cpf;
+        this.cpf = cpf;
+        propertyChangeSupport.firePropertyChange("cpf", oldCPF, cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idpaciente != null ? idpaciente.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Paciente)) {
+            return false;
+        }
+        Paciente other = (Paciente) object;
+        if ((this.idpaciente == null && other.idpaciente != null) || (this.idpaciente != null && !this.idpaciente.equals(other.idpaciente))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "bean.Paciente[ idpaciente=" + idpaciente + " ]";
+    }
+    
+}
